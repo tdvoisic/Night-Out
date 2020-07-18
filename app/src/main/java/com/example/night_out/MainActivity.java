8edbb3d46994a29732a18813add6b6caa9472a95
Tim Voisich <tdvoisic@buffalo.edu>
2020-07-18 03:48:24 -0400

    Temp logout with password field hidden

--- app/src/main/java/com/example/night_out/MainActivity.java
+++ app/src/main/java/com/example/night_out/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.night_out;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth UserAuth;
+    private Button LogOutTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UserAuth = FirebaseAuth.getInstance();
+
+        LogOutTemp = (Button) findViewById(R.id.logout_temp);
+
+        LogOutTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserAuth.signOut();
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
     }
 }
