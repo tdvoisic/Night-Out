e2643c453d1dbf25b06e6b9e9a091133f77155e8
Tim Voisich <tdvoisic@buffalo.edu>
2020-07-19 22:32:48 -0400

    Created bottom nav bar with basic activities

--- app/src/main/java/com/example/night_out/NotificationsActivity.java
+++ app/src/main/java/com/example/night_out/NotificationsActivity.java
@@ -5,29 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth UserAuth;
-    private Button LogOutTemp;
+public class NotificationsActivity extends AppCompatActivity {
 
     private BottomNavigationView Nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        UserAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_notifications);
 
-        Nav = findViewById(R.id.main_bottom_nav);
-        Nav.setSelectedItemId(R.id.nav_home);
+        Nav = findViewById(R.id.notify_bottom_nav);
+        Nav.setSelectedItemId(R.id.nav_notifications);
 
         Nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -36,30 +28,16 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
-        LogOutTemp = (Button) findViewById(R.id.logout_temp);
-
-        LogOutTemp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserAuth.signOut();
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(loginIntent);
-                finish();
-            }
-        });
     }
 
     private void UserMenuSelector(MenuItem menuItem) {
         switch (menuItem.getItemId()){
             case R.id.nav_home:
-                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0,0);
                 break;
             case R.id.nav_notifications:
-                startActivity(new Intent(getApplicationContext(), NotificationsActivity.class));
-                overridePendingTransition(0,0);
+                //startActivity(new Intent(getApplicationContext(), NotificationsActivity.class));
                 break;
             case R.id.nav_discover:
                 startActivity(new Intent(getApplicationContext(), DiscoverActivity.class));
